aa3ed708a8c555571abf2ad30d39c11df7313bf6
springlin2012@gmail.com <lichunlin@xiaoniu66.com>
2018-03-19 11:54:33 +0800

    update

--- src/main/java/com/io/study/ch03/Marketing.java
+++ src/main/java/com/io/study/ch03/Marketing.java
@@ -6,8 +6,15 @@
  */
 package com.io.study.ch03;
 
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.GatheringByteChannel;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 /**
- * 功能描述: XXXX
+ * 功能描述: Demo Scatter/Gather
  * <p/>
  * 创建人: chunlin.li
  * <p/>
@@ -16,4 +23,62 @@ package com.io.study.ch03;
  * Copyright (c) 深圳市牛鼎丰科技有限公司-版权所有
  */
 public class Marketing {
+
+    private static final String DEMOGRAPHIC = "files/blahblah.txt";
+
+
+    public static void main(String[] args) throws Exception {
+        int reps = 10;
+        if (args.length > 0) {
+            reps = Integer.parseInt(args[0]);
+        }
+        FileOutputStream fos = new FileOutputStream(DEMOGRAPHIC);
+        GatheringByteChannel gatherChannel = fos.getChannel();
+
+        ByteBuffer[] bs = utterBS(reps);
+        while (gatherChannel.write(bs) > 0) {
+
+        }
+        System.out.println("Mindshare paradigms synergized to " + DEMOGRAPHIC);
+        fos.close();
+    }
+
+    // These are just representative; add your own
+
+    private static String[] col1 = {"Aggregate", "Enable", "Leverage", "Facilitate", "Synergize", "Repurpose", "Strategize", "Reinvent", "Harness"};
+    private static String[] col2 = {"cross-platform", "best-of-breed", "frictionless", "ubiquitous", "extensible", "compelling", "mission-critical", "collaborative", "integrated"};
+    private static String[] col3 = {"methodologies", "infomediaries", "platforms", "schemas", "mindshare", "paradigms", "functionalities", "web services", "infrastructures"};
+
+    // 系统换行分割符
+    private static String newline = System.getProperty("line.separator");
+
+    // 从三个数组中随机抽取每一个数组的其中一个属性放入List
+    // List 转为字节数组返回
+    private static ByteBuffer[] utterBS(int howMany) throws Exception {
+        List list = new LinkedList();
+        for (int i = 0; i < howMany; i++) {
+            list.add(pickRandom(col1, " "));
+            list.add(pickRandom(col2, " "));
+            list.add(pickRandom(col3, newline));
+        }
+        ByteBuffer[] bufs = new ByteBuffer[list.size()];
+        list.toArray(bufs);
+        return (bufs);
+    }
+
+
+    // The communications director
+    private static Random rand = new Random();
+
+    // 读取字符串, 编码后放入 "字节流"
+    private static ByteBuffer pickRandom(String[] strings, String suffix) throws Exception {
+        String string = strings[rand.nextInt(strings.length)];
+        int total = string.length() + suffix.length();
+        ByteBuffer buf = ByteBuffer.allocate(total);
+
+        buf.put(string.getBytes("US-ASCII"));
+        buf.put(suffix.getBytes("US-ASCII"));
+        buf.flip();
+        return (buf);
+    }
 }
